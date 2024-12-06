472972df1508c118dd9abcf658ac461958166bc6
Tryanin-it-18 <daniiltranin@gmail.com>
2024-12-06 10:27:30 +0500

    Update крестики-нолики.cpp

--- крестики-нолики.cpp
+++ крестики-нолики.cpp
@@ -1,4 +1,3 @@
-// Программа на C++ для реализации игры в крестики-нолики
 #include <iostream>
 using namespace std;
 
@@ -15,44 +14,39 @@ void drawBoard(char board[3][3]) {
 }
 
 // Функция для проверки на победу
-bool checkWin(char board[3][3], char player)
-{
+bool checkWin(char board[3][3], char player) {
     // Проверка строк, столбцов и диагоналей
     for (int i = 0; i < 3; i++) {
-        if (board[i][0] == player && board[i][1] == player
-            && board[i][2] == player)
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
             return true;
-        if (board[0][i] == player && board[1][i] == player
-            && board[2][i] == player)
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
             return true;
     }
-    if (board[0][0] == player && board[1][1] == player
-        && board[2][2] == player)
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
         return true;
-    if (board[0][2] == player && board[1][1] == player
-        && board[2][0] == player)
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
         return true;
     return false;
 }
 
-int main()
-{
+int main() {
+    setlocale(LC_ALL, "RU");
     // Инициализация поля и игроков
     char board[3][3] = { { ' ', ' ', ' ' },
                          { ' ', ' ', ' ' },
                          { ' ', ' ', ' ' } };
-    
+
     char player1, player2;
     cout << "Игрок 1, выберите символ (X или O): ";
     cin >> player1;
-    
+
     // Проверим, чтобы игрок 2 получил противоположный символ
-    player2 = (player1 == 'X') ? 'O' : 'X';
+    player2 = (player1 == 'X' || player1 == 'x') ? 'O' : 'X';
     cout << "Игрок 2, ваш символ: " << player2 << endl;
 
     char currentPlayer = player1; // Текущий игрок
     int row, col;
-    int turn; // Объявляем переменную для количества ходов
+    int turn; // Переменная для количества ходов
 
     cout << "Добро пожаловать в Крестики-Нолики!\n";
 
@@ -63,13 +57,18 @@ int main()
 
         // Запрос ввода
         while (true) {
-            cout << "Игрок " << currentPlayer
-                 << ", введите строку (0-2) и столбец (0-2): ";
+            cout << "Игрок " << currentPlayer << ", введите через пробел строку (1-3) и столбец (1-3): ";
             cin >> row >> col;
 
-            if (board[row][col] != ' ' || row < 0 || row > 2
-                || col < 0 || col > 2) {
-                cout << "Неверный ход. Попробуйте снова.\n";
+            // Приведение ввода к индексам массива (0-2)
+            row--;
+            col--;
+
+            if (row < 0 || row > 2 || col < 0 || col > 2) {
+                cout << "Неверные координаты. Укажите числа от 1 до 3.\n";
+            }
+            else if (board[row][col] != ' ') {
+                cout << "Эта клетка уже занята. Попробуйте снова.\n";
             }
             else {
                 break; // Корректный ввод, выходим из цикла.
@@ -83,7 +82,7 @@ int main()
         if (checkWin(board, currentPlayer)) {
             drawBoard(board);
             cout << "Игрок " << currentPlayer << " победил!\n";
-            break; // Выход из цикла после победы.
+            return 0; // Выход из программы после победы.
         }
 
         // Переключение на другого игрока
@@ -94,9 +93,6 @@ int main()
     drawBoard(board);
 
     // Проверка на ничью
-    if (turn == 9 && !checkWin(board, player1) && !checkWin(board, player2)) {
-        cout << "Ничья!\n";
-    }
-
+    cout << "Ничья!\n";
     return 0; // Выход из программы
 }
